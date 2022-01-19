bd874231ee27812f353f71debf4dd5def9ef10ff
Julien Deveaux <julien.deveaux@hotmail.com>
2022-01-19 12:05:11 +0100

    Ajout dgs cours et autoroute

--- Readme.md
+++ Readme.md
@@ -2,4 +2,4 @@
    graphe dans GS, Résolvez le problème de flot max et vérifiez à la main la solution obtenue.
    1. Le résultat obtenu semble correct, le flot max serait de 15 alors que les deux derniers liens ne peuvent supporter ue 8 et 7 de flots. Donc 15 max dans le réseau.
 2. Testez avec un second exemple (du cours ou construit par vous):
-   1. 
+   1. Le second exemple du cours (premier exemple cours flots), fonctionne comme prévu.
--- src/main/java/maxflow/TestMaxFlow.java
+++ src/main/java/maxflow/TestMaxFlow.java
@@ -3,9 +3,22 @@ package maxflow;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.GraphParseException;
+
+import java.io.IOException;
 
 public class TestMaxFlow
 {
+    protected static String styleSheet =
+            "node {" +
+                    "   text-size: 20px;" +
+                    "   text-alignment: above;" +
+                    "   fill-mode: gradient-radial;" +
+                    "}" +
+                    "edge {" +
+                    "   text-alignment: above;" +
+                    "   text-size: 20px;" +
+                    "}";
 
     public static void main(String[] args)
     {
@@ -21,7 +34,7 @@ public class TestMaxFlow
         g.addEdge("e3", "1", "2", true).setAttribute("cap", 5);
         g.addEdge("e4", "1", "t", true).setAttribute("cap", 8);
         g.addEdge("e5", "2", "t", true).setAttribute("cap", 7);
-        g.display();
+        //g.display();
 
         MaxFlow mf = new MaxFlow();
         mf.setCapacityAttribute("cap");
@@ -45,6 +58,56 @@ public class TestMaxFlow
             if (flow > 0) e.setAttribute("ui.label", "" + flow);
             if (cap == flow) e.setAttribute("ui.style", "fill-color: red;");
         });
+
+        Graph graphCours = new SingleGraph("test cours");
+        graphCours.setAttribute("ui.stylesheet", styleSheet);
+        try {
+            graphCours.read("src/main/resources/exempleCours.dgs");
+        } catch (IOException | GraphParseException e) {
+            e.printStackTrace();
+        }
+        mf.init(graphCours);
+        mf.setSource(graphCours.getNode("s"));
+        mf.setSink(graphCours.getNode("t"));
+        mf.compute();
+
+        System.out.println(mf.getFlow());
+
+        graphCours.edges().forEach( e ->
+        {
+            double flow = mf.getFlow(e);
+            double cap = mf.getCapacity(e);
+            if (flow > 0) e.setAttribute("ui.label", "" + flow);
+            if (cap == flow) e.setAttribute("ui.style", "fill-color: red;");
+        });
+
+        //graphCours.display();
+
+
+        Graph reseau = new SingleGraph("autoroute");
+        reseau.setAttribute("ui.stylesheet", styleSheet);
+        try {
+            reseau.read("src/main/resources/reseau.dgs");
+        } catch (IOException | GraphParseException e) {
+            e.printStackTrace();
+        }
+
+        mf.init(reseau);
+        mf.setSource(reseau.getNode("A"));
+        mf.setSink(reseau.getNode("I"));
+        mf.compute();
+
+        System.out.println(mf.getFlow());
+
+        reseau.edges().forEach( e ->
+        {
+            double flow = mf.getFlow(e);
+            double cap = mf.getCapacity(e);
+            if (flow > 0) e.setAttribute("ui.label", "" + flow);
+            if (cap == flow) e.setAttribute("ui.style", "fill-color: red;");
+        });
+
+        reseau.display();
     }
 
 }
