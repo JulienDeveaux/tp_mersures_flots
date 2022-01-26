62f6abb2cb16327ad95afb79c7dc97f35c605892
Dim145 <dimitri.bleach@gmail.com>
2022-01-26 17:10:10 +0100

    ajout du brut force des flow max pour les liens

--- src/main/java/maxflow/TestMaxFlow.java
+++ src/main/java/maxflow/TestMaxFlow.java
@@ -9,6 +9,7 @@ import org.graphstream.stream.GraphParseException;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class TestMaxFlow
 {
@@ -36,12 +37,9 @@ public class TestMaxFlow
 
         MaxFlow mf = new MaxFlow();
 
-        mf.init(reseau);
-        mf.setSource(reseau.getNode("A"));
-        mf.setSink(reseau.getNode("I"));
-        mf.compute();
+        double flowAI = getFlowMaxBetweenAandI(reseau, mf);
 
-        System.out.println(mf.getFlow());
+        System.out.println(flowAI);
 
         reseau.edges().forEach( e ->
         {
@@ -53,6 +51,7 @@ public class TestMaxFlow
             if (cap == flow) e.setAttribute("ui.style", "fill-color: red;");
         });
 
+        brutForceX4(reseau, 4);
         //AfficheurGraph.defaultViewWithIdTitle(reseau);
 
         Graph reseauEcart = new SingleGraph("Graph d'écart autoroute");
@@ -101,6 +100,123 @@ public class TestMaxFlow
         frameAutoroute.setVisible(true);
 
         //AfficheurGraph.defaultView(reseauEcart, "Réseau écart (Autoroute)");
+    }
+
+    private static double getFlowMaxBetweenAandI(Graph g, MaxFlow mf)
+    {
+        mf.init(g);
+        mf.setSource(g.getNode("A"));
+        mf.setSink(g.getNode("I"));
+        mf.compute();
+
+        return mf.getFlow();
+    }
+
+    /**
+     * Tests chacun des cas avec la solution optimal.
+     * Le nombre d'arrete de passé est le nb max de liens du tableau qui seront gardée après chaque exécution.
+     * <br/><br/>
+     * A chaques nouvelle itération, le lien qui donne le flow max est gardée et supprimer des liens possible pour l'itération suivante
+     * @param g le graph qui seras utilisé ET modifier lors de l'éxécution
+     * @param nbArrete le nombre de nouveaux lien à gardée
+     */
+    private static void brutForceX4(Graph g, int nbArrete)
+    {
+        String[] nvRoutes = new String[]{ "A_C", "A_H", "A_D", "A_I", "C_D", "C_H", "C_I", "D_H", "D_I", "H_I" };
+
+        nbArrete = Math.min(Math.max(nbArrete, 0), nvRoutes.length);
+
+        String[][] liensOpti = new String[nbArrete][2];
+
+        for (int i = 0; i < nbArrete; i++)
+        {
+            double[] max = brutForce(g, nvRoutes);
+
+            liensOpti[i][0] = nvRoutes[(int) max[0]];
+            liensOpti[i][1] = String.valueOf(max[1]);
+
+            String[] route = liensOpti[i][0].split("_");
+
+            Edge e = g.getEdge(route[0] + "-" + route[1]);
+
+            boolean isUpdate = e != null;
+
+            if(isUpdate)
+            {
+                double ancienne = (double) e.getAttribute("cap");
+
+                e.setAttribute("cap", ancienne + 2500);
+                e.setAttribute("ui.label", ancienne + 2500);
+            }
+            else
+            {
+                e = g.addEdge(nvRoutes[i], route[0], route[1], true);
+                e.setAttribute("cap", 2500.0);
+                e.setAttribute("ui.label", 2500.0);
+            }
+
+            int finalI = i;
+            nvRoutes = Arrays.stream(nvRoutes).filter(s -> !s.equals(liensOpti[finalI][0])).toList().toArray(new String[0]);
+        }
+
+        System.out.println("--- résulats ---");
+
+        for (String[] tab : liensOpti)
+            System.out.println(tab[0] + " : " + tab[1]);
+    }
+
+    /**
+     *
+     * @param g le graph sur lequel les liens seront ajouter (temporairement)
+     * @param nvRoutes un tableau des nouvelles routes (ou maj si existante) chaque string sous forme ID1_ID2 ou ID1 represent l'id su noeud source
+     * @return un tableau à 2 case, la première donne l'indice de la route max dans le tableau et la seconde la valeur du flow max avec cette route
+     */
+    private static double[] brutForce(Graph g, String[] nvRoutes)
+    {
+        double[] nvValues = new double[nvRoutes.length];
+
+        for (int i = 0; i < nvRoutes.length; i++)
+        {
+            String[] route = nvRoutes[i].split("_");
+
+            Edge e = g.getEdge(route[0] + "-" + route[1]);
+
+            boolean isUpdate = e != null;
+
+            if(isUpdate)
+            {
+                double ancienne = (double) e.getAttribute("cap");
+
+                e.setAttribute("cap", ancienne + 2500);
+            }
+            else
+            {
+                e = g.addEdge(nvRoutes[i], route[0], route[1], true);
+                e.setAttribute("cap", 2500.0);
+            }
+
+            nvValues[i] = getFlowMaxBetweenAandI(g, new MaxFlow());
+
+            if(isUpdate)
+            {
+                double nouvelle = (double) e.getAttribute("cap");
+
+                e.setAttribute("cap", nouvelle - 2500);
+            }
+            else
+            {
+                g.removeEdge(e);
+            }
+        }
+
+        double max = Arrays.stream(nvValues).max().orElse(0);
+        int indice = 0;
+
+        for (int i = 0; i < nvValues.length; i++)
+            if(nvValues[i] == max)
+                indice = i;
+
+        return new double[]{ indice, max };
     }
 
     private static void testAndExempleCours() throws IOException, GraphParseException
